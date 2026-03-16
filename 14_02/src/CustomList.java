502d63ab21b706fc5f6899e8c17436fcf2d9ae79
sdnytmt <ooaa0157@gmail.com>
2026-03-16 15:59:36 +0300

    14_02 homework done

--- 14_02/src/CustomList.java
+++ 14_02/src/CustomList.java
@@ -59,4 +59,56 @@ public class CustomList {
         }
         pointer.nextNode = null;
     }
+
+    public void reverse(){
+        Node pointer = start;
+        Node prev = null;
+        Node next;
+        while (pointer != null){
+            next = pointer.nextNode;
+            pointer.nextNode = prev;
+            prev = pointer;
+            pointer = next;
+        }
+        start = prev;
+    }
+
+    public int length(){
+        Node pointer = start;
+        int count = 0;
+        while (pointer!=null){
+            count+=1;
+            pointer = pointer.nextNode;
+        }
+        return count;
+    }
+
+    public boolean isEmpty(){
+        return start == null;
+    }
+
+    public boolean isInList(int element){
+        Node pointer = start;
+        while (pointer != null){
+            if (pointer.info == element){
+                return true;
+            }
+            pointer = pointer.nextNode;
+        }
+        return false;
+    }
+
+    public void getIndex(int element){
+        Node pointer = start;
+        int counter = 0;
+
+        while (pointer != null){
+            if (pointer.info == element){
+                System.out.print(counter + ", ");
+            }
+            pointer = pointer.nextNode;
+            counter += 1;
+        }
+        System.out.println();
+    }
 }
